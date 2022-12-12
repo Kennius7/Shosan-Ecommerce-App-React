a1be37cf067f83382d94aaf64d7ee9a73eb66e3e
Kenniusboggs <ogbogukenny@yahoo.com>
2022-12-12 17:12:54 +0100

    Further styling

--- src/components/menu-item/Menu-Item.jsx
+++ src/components/menu-item/Menu-Item.jsx
@@ -3,7 +3,8 @@ import './Menu-Item.scss';
 
 function MenuItem ({ imageUrl, size, title }) {
     return (
-        <div className={`${size} menu-item`} style={{backgroundImage:`url(${imageUrl})`}}>
+        <div className={`${size} menu-item`}>
+            <div className='background-image' style={{backgroundImage:`url(${imageUrl})`}} />
             <div className='content'>
                 <h1 className='title'>{ title }</h1>
                 <span className='subtitle'>SHOP NOW</span>
